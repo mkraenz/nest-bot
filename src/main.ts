b87c0ef91cf0b76dfec1465efa612ae17b44a5ac
Mirco Kraenz <kraenzmirco@gmail.com>
2019-02-08 16:06:37 +0900

    CLEANUP #1 comments in main.ts

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,7 @@ import { resolve } from 'path';
 import { AppModule } from './app.module';
 
 async function startServer() {
-    // TODO https https://github.com/nestjs/nest/issues/113
-    // tslint:disable-next-line:max-line-length
-    // https://medium.freecodecamp.org/how-to-get-https-working-on-your-local-development-environment-in-5-minutes-7af615770eec
+    // TODO use https see nestjs doc FAQ
     const app = await NestFactory.create(AppModule);
 
     // TODO forbidUnknownValues not working https://github.com/typestack/class-validator/issues/305
