a8215fe1456de1ed727f3b12e1596dd846bc3acf
Mirco Kraenz <kraenzmirco@gmail.com>
2019-02-08 16:02:57 +0900

    UPDATE #1 replace own logger middleware by custom Morgan

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,11 @@
 import { CookieParserMiddleware } from '@nest-middlewares/cookie-parser';
 import { MorganMiddleware } from '@nest-middlewares/morgan';
 import { MiddlewareConsumer, Module, RequestMethod } from '@nestjs/common';
+import { RouteInfo } from '@nestjs/common/interfaces';
 import { APP_GUARD } from '@nestjs/core';
 import { AuthModule } from './auth/auth.module';
 import { BotModule } from './bot/bot.module';
 import { CatsModule } from './cats/cats.module';
-import { loggerMiddleware } from './logger.middleware';
 import { MockPricesModule } from './mock-prices/mock-prices.module';
 import { RolesGuard } from './roles-guard/roles.guard';
 import { RootModule } from './root/root.module';
@@ -29,10 +29,21 @@ import { UserModule } from './users/users.module';
 })
 export class AppModule {
     public configure(consumer: MiddlewareConsumer) {
-        const ALL = { path: '*', method: RequestMethod.ALL };
-        consumer.apply(loggerMiddleware).forRoutes(ALL);
+        const ALL: RouteInfo = { path: '*', method: RequestMethod.ALL };
         consumer.apply(CookieParserMiddleware).forRoutes(ALL);
-        MorganMiddleware.configure('tiny');
-        consumer.apply(MorganMiddleware).forRoutes(ALL);
+        this.applyCustomMorganMiddleWare();
+        consumer
+            .apply(MorganMiddleware)
+            .exclude({ path: 'prices', method: RequestMethod.ALL })
+            .forRoutes(ALL);
+    }
+
+    private applyCustomMorganMiddleWare() {
+        MorganMiddleware.configure(
+            ':method :url :status :res[content-length] - :response-time ms :body',
+        );
+        MorganMiddleware.token('body', (req, res) => {
+            return JSON.stringify({ reqBody: req.body });
+        });
     }
 }
--- src/logger.middleware.ts
+++
@@ -1,8 +0,0 @@
-export async function loggerMiddleware(req, res, next) {
-    // tslint:disable-next-line:no-console
-    console.log({
-        reqUrl: req.originalUrl,
-        reqBody: req.body,
-    });
-    next();
-}
