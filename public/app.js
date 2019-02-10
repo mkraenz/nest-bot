9773f18a77e1914863495b24ad15d8fcb5b8b092
Mirco Kraenz <kraenzmirco@gmail.com>
2019-02-10 16:20:52 +0900

    ADD #1 frontend charts for mva and price

---
+++ public/app.js
@@ -0,0 +1,65 @@
+function renderWeatherChart(weatherData) {
+    var ctx = document.getElementById('weatherChart').getContext('2d');
+    var options = {};
+    weatherChartRef = new Chart(ctx, {
+        type: 'line',
+        data: weatherData,
+        options: options,
+    });
+}
+
+var chartConfig = {
+    labels: [],
+    datasets: [],
+};
+
+function getChartConfig(priceData, mvaData) {
+    chartConfig.labels = [...Array(priceData.length).keys()];
+    chartConfig.datasets.push(getDataSetEntry('Prices', priceData, Color.Blue));
+    chartConfig.datasets.push(getDataSetEntry('mva', mvaData, Color.Red));
+}
+
+async function main() {
+    try {
+        const host = 'http://localhost:3000';
+        const response = await fetch(`${host}/price`);
+        const priceData = await response.json();
+        const mvaResponse = await fetch(`${host}/moving-averages`);
+        const mvaData = await mvaResponse.json();
+        getChartConfig(priceData, mvaData);
+        renderWeatherChart(chartConfig);
+    } catch (error) {
+        console.log(error);
+    }
+}
+main();
+
+function getDataSetEntry(label, data, color) {
+    return {
+        label,
+        data,
+        fill: false,
+        lineTension: 0.1,
+        backgroundColor: color,
+        borderColor: color,
+        borderCapStyle: 'butt',
+        borderDash: [],
+        borderDashOffset: 0.0,
+        borderJoinStyle: 'miter',
+        pointBorderColor: color,
+        pointBackgroundColor: '#fff',
+        pointBorderWidth: 1,
+        pointHoverRadius: 5,
+        pointHoverBackgroundColor: color,
+        pointHoverBorderColor: 'rgba(220,220,220,1)',
+        pointHoverBorderWidth: 2,
+        pointRadius: 1,
+        pointHitRadius: 10,
+        spanGaps: false,
+    };
+}
+
+const Color = {
+    Blue: 'rgba(75,192,192,1)',
+    Red: 'rgba(255,0,0)',
+};
